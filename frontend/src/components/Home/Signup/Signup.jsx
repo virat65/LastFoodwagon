abe8afd8553be86fe3441b02f149ffdfcf66356f
virat65 <virat1322420@gmail.com>
2025-08-23 21:37:07 +0530

    signup

--- frontend/src/components/Home/Signup/Signup.jsx
+++ frontend/src/components/Home/Signup/Signup.jsx
@@ -1,5 +1,70 @@
-return (
-  <div className="container-fluid min-vh-100 d-flex justify-content-center align-items-center">
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import API from "../../login/backendrouting";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+const Singup = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    image: "",
+    userType: "",
+    secreatekey: "",
+  });
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    console.log(files);
+    if (name === "image") {
+      setData({ ...data, [name]: files[0] });
+    } else {
+      setData({ ...data, [name]: value });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      data?.userType === "Admin" &&
+      data.secreatekey != import.meta.env.VITE_adminkey
+    ) {
+      toast.error("Invalid Amin");
+    } else {
+      try {
+        const dataaaa = new FormData();
+        dataaaa.append("userType", data.userType);
+        if (data.userType === "Admin") {
+          dataaaa.append("secreatekey", dataaaa.secreatekey);
+        }
+        const inputfield = data.email.trim();
+        const isPhone = /^[0-9]{10}/.test(inputfield);
+        if (isPhone) {
+          dataaaa.append("phone", inputfield);
+        } else {
+          dataaaa.append("email", data.email);
+        }
+        dataaaa.append("name", data.name);
+
+        dataaaa.append("password", data.password);
+        dataaaa.append("image", data.image);
+        const formData = await axios.post(API.signup.url, dataaaa);
+        console.log(formData, "formData");
+        if (formData.data.status == 200) {
+          toast.success(formData.data.message);
+          navigate("/login");
+        } else {
+          toast.error(formData.data.message);
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error(error);
+      }
+    }
+  };
+  return (
+<div className="container-fluid min-vh-100 d-flex justify-content-center align-items-center">
     <div className="row w-100">
       <div className="col-12 col-sm-10 col-md-6 col-lg-4 mx-auto">
         <div className="card p-4 shadow">
@@ -99,4 +164,8 @@ return (
       </div>
     </div>
   </div>
-);
+
+  );
+};
+
+export default Singup;
